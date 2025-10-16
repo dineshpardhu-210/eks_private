8c2aba32704deb3af83d5e8ea930dedc5a5572cf
dineshpardhu-210 <dineshpardhu@gmail.com>
2025-10-16 11:39:40 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,1 +1,25 @@
+resource "aws_eks_cluster" "this" {
+  name = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access = false
+  }
+
+  # kubeconfig-related outputs are available via data sources in root if needed
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name = aws_eks_cluster.this.name
+  node_group_name = "ng-${var.cluster_name}"
+  node_role_arn = var.node_role_arn
+  subnet_ids = var.private_subnet_ids
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    max_size = var.node_group_desired_size + 1
+    min_size = 1
+  }
+}
 
